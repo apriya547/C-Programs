3a1d0bf5378b05e5335b1b74ce37e9f2c2236555
apriya547 <42911245+apriya547@users.noreply.github.com>
2019-05-06 14:03:48 +0530

    Add files via upload

---
+++ 4.ReadNmaeandPrint.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name[30];
+    printf("enter the name");
+    scanf("%s",&name);
+    printf("you entered the name:%s",name);
+    getch();
+
+}
