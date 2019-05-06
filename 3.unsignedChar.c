abb76cb9d09db0008b86515ba227706b5ca9657b
apriya547 <42911245+apriya547@users.noreply.github.com>
2019-05-06 14:00:01 +0530

    Add files via upload

---
+++ 3.unsignedChar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    unsigned int distance;
+    printf("enter the distance");
+    scanf("%u",&distance);
+    printf("the distance is:%u",distance);
+    getch();
+
+}
