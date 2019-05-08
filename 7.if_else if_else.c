1b992b4fb36a959d10c8e93b86d88e4ac7b90e7d
apriya547 <42911245+apriya547@users.noreply.github.com>
2019-05-08 18:15:37 +0530

    Add files via upload

---
+++ 7.if_else if_else.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n==0)
+
+        printf("number is zero",n);
+    else if(n%2==0)
+        printf("%d is odd number",n);
+    else
+        printf("%d is odd number",n);
+
+
+    getch();
+}
+
