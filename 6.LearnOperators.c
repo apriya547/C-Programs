ee55c04b8cc8d7a8db1b65df3793dd038bc9a64f
apriya547 <42911245+apriya547@users.noreply.github.com>
2019-05-08 17:47:24 +0530

    Add files via upload

---
+++ 6.LearnOperators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=6,b=3;
+    printf("%d AND %d=%d",a,b,a&b);
+    printf("%d OR %d=%d",a,b,a|b);
+    printf("%d XOR %d=%d",a,b,a^b);
+    getch();
+
+
+}
